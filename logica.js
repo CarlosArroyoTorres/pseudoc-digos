e703c0f863ec2f20aff99834fee190a479b23e7a
Carlos Emmanuel Arroyo Torres <ceat1989.ca@gmail.com>
2025-06-23 20:31:15 -0600

    Add files via upload

---
+++ logica.js
@@ -0,0 +1,66 @@
+function compararEdad() {
+    let personaUno = document.getElementById('nombreUno').value;
+    let personaDos = document.getElementById('nombreDos').value;
+    let personaTres = document.getElementById('nombreTres').value;
+
+    let edadUno = parseInt(document.getElementById('edadUno').value);
+    let edadDos = parseInt(document.getElementById('edadDos').value);
+    let edadTres = parseInt(document.getElementById('edadTres').value);
+
+    let mensaje = document.getElementById('decision');
+
+    if (edadUno >= edadDos && edadUno >= edadTres) {
+        mensaje.innerText = `${personaUno}, tú eres el mayor y tu edad es ${edadUno}`;
+    } else if (edadDos >= edadUno && edadDos >= edadTres) {
+        mensaje.innerText = `${personaDos}, tú eres el mayor y tu edad es ${edadDos}`;
+    } else {
+        mensaje.innerText = `${personaTres}, tú eres el mayor y tu edad es ${edadTres}`;
+    }
+}
+
+function sugerirRegalo() {
+    let dinero = parseFloat(document.getElementById("dinero").value);
+    let resultado = document.getElementById("resultado");
+    if (isNaN(dinero) || dinero < 0) {
+        resultado.innerText = "Por favor, ingresa una cantidad válida.";
+    } else if (dinero <= 10) {
+         resultado.innerText = "Puedes comprar una tarjeta.";
+    } else if (dinero >= 11 && dinero <= 100) {
+        resultado.innerText = "Puedes comprar chocolates.";
+    } else if (dinero >= 101 && dinero <= 250) {
+        resultado.innerText = "Puedes comprar flores.";
+    } else if (dinero > 251) {
+        resultado.innerText = "Puedes comprar un anillo.";
+    } else {
+        resultado.innerText = "No hay sugerencia para este monto.";
+    }
+}
+function calcularCosto() {
+    let horas = parseInt(document.getElementById("horas").value);
+    let resultado = document.getElementById("resultado");
+    let costo = 0;
+    if (isNaN(horas) || horas < 0) {
+        resultado.innerText = "Por favor, ingresa una cantidad válida de horas.";
+        return;
+    }
+    if (horas <= 2) {
+        costo = horas * 5;
+    } else {
+        costo += 2 * 5; // Primeras 2 horas
+    let horasRestantes = horas - 2;
+    if (horasRestantes <= 3) {
+        costo += horasRestantes * 4;
+    } else {
+        costo += 3 * 4; // Siguientes 3 horas
+        horasRestantes -= 3;
+    if (horasRestantes <= 5) {
+        costo += horasRestantes * 3;
+    } else {
+        costo += 5 * 3; // Siguientes 5 horas
+        horasRestantes -= 5;
+    costo += horasRestantes * 2; // Resto de horas a $2
+    }
+    }
+    }
+    resultado.innerText = `El total a pagar es: ₡${costo.toFixed(2)}`;
+}
