4ef0d80d99e125df02b6df7dbc3e070bc480855e
Carlos Emmanuel Arroyo Torres <ceat1989.ca@gmail.com>
2025-06-30 20:10:11 -0600

    Add files via upload

--- logica.js
+++ logica.js
@@ -24,7 +24,7 @@ function sugerirRegalo() {
     if (isNaN(dinero) || dinero < 0) {
         resultado.innerText = "Por favor, ingresa una cantidad válida.";
     } else if (dinero <= 10) {
-         resultado.innerText = "Puedes comprar una tarjeta.";
+        resultado.innerText = "Puedes comprar una tarjeta.";
     } else if (dinero >= 11 && dinero <= 100) {
         resultado.innerText = "Puedes comprar chocolates.";
     } else if (dinero >= 101 && dinero <= 250) {
@@ -47,20 +47,40 @@ function calcularCosto() {
         costo = horas * 5;
     } else {
         costo += 2 * 5; // Primeras 2 horas
-    let horasRestantes = horas - 2;
-    if (horasRestantes <= 3) {
-        costo += horasRestantes * 4;
-    } else {
-        costo += 3 * 4; // Siguientes 3 horas
-        horasRestantes -= 3;
-    if (horasRestantes <= 5) {
-        costo += horasRestantes * 3;
-    } else {
-        costo += 5 * 3; // Siguientes 5 horas
-        horasRestantes -= 5;
-    costo += horasRestantes * 2; // Resto de horas a $2
-    }
-    }
+        let horasRestantes = horas - 2;
+        if (horasRestantes <= 3) {
+            costo += horasRestantes * 4;
+        } else {
+            costo += 3 * 4; // Siguientes 3 horas
+            horasRestantes -= 3;
+            if (horasRestantes <= 5) {
+                costo += horasRestantes * 3;
+            } else {
+                costo += 5 * 3; // Siguientes 5 horas
+                horasRestantes -= 5;
+                costo += horasRestantes * 2; // Resto de horas a $2
+            }
+        }
     }
     resultado.innerText = `El total a pagar es: ₡${costo.toFixed(2)}`;
 }
+function calcularBono() {
+    const salarioMinino = 1000;
+    let nombreP = document.getElementById('nombreProfesor').value;
+    let puntosP = parseFloat(document.getElementById('puntosProfesor').value);
+    let textoContenido = document.getElementById('decision');
+
+
+    if (puntosP > 151) {
+        bono = salarioMinino * 3;
+        textoContenido.textContent = `Su nombre es ${nombreP} y su bono es: $${bono}`;
+    } else if (puntosP >= 101 && puntosP <= 150) {
+        bono = salarioMinino * 2;
+        textoContenido.textContent = `su nombre es ${nombreP} y su bono es: $${bono}`;
+    }
+    else if (puntosP >= 0 && puntosP < 100) {
+        bono = salarioMinino * 1;
+        textoContenido.textContent = `su nombre es ${nombreP} y su bono es: $${bono}`;
+    }
+}
+
